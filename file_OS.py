d8c9502004573d580a7016b9b0752fca246a8124
Mikhail <t2oyz@mail.ru>
2024-10-25 11:08:25 +0300

    Add files via upload

---
+++ file_OS.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+directory = "." # Текущая директория
+
+#обход каталога
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        #путь к файлу
+        filepath = os.path.join(root, file)
+
+        #время изменения файла
+        filetime = os.path.getmtime(filepath)
+
+        #Форматируем время изменения
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+        #размер файла
+        filesize = os.path.getsize(filepath)
+
+        #оснавная директорию файла
+        parent_dir = os.path.dirname(filepath)
+
+        #информацию о файле
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
