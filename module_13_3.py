7f14abf50685906b2cc883125e8c01c980f8c5d3
Mikhail <t2oyz@mail.ru>
2024-11-11 20:01:21 +0300

    Add files via upload

--- module_13_3.py
+++ module_13_3.py
@@ -1,21 +1,59 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = ''
+api = '7809070886:AAG7LEYnKzkVEaRkBfjJgCGMoMvDkBLzcnM'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
 @dp.message_handler(commands=['start'])
-async def start(message):
-    print("Привет! я бот помогающий твоему здоровью")
+async def start(message: types.Message):
     await message.answer("Привет! я бот помогающий твоему здоровью")
 
 
+@dp.message_handler(text='Calories')
+async def set_age(message: types.Message, state: FSMContext):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
+    calories = (10 * weight) + (6.25 * growth) - (5 * age) + 5
+    await message.reply(f"Ваша норма калорий {calories}")
+    await state.finish()
+
+
 @dp.message_handler()
-async def all_message(message):
-    print("Введите команду /start, чтобы начать общение.")
+async def all_message(message: types.Message):
     await message.answer("Введите команду /start, чтобы начать общение.")
 
 
