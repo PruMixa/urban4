1e92e48c97916b53d2cbbd7c8baf09e183f3f316
Mikhail <t2oyz@mail.ru>
2024-11-12 19:13:19 +0300

    module_13_6.py

--- module_13_6.py
+++ module_13_6.py
@@ -6,7 +6,7 @@ import asyncio
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-api = '7809070886:AAG7LEYnKzkVEaRkBfjJgCGMoMvDkBLzcnM'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
